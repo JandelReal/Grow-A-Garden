586c9f91a4dc2430197fb6a8ff344f6db3eafaaa
JandelReal <beluga69420123@gmail.com>
2025-08-01 15:25:00 +0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBO3h_iI7SHhZFytdHc3nKhddlxAPKwRIs",
+  authDomain: "grow-a-garden-dfdab.firebaseapp.com",
+  databaseURL: "https://grow-a-garden-dfdab-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "grow-a-garden-dfdab",
+  storageBucket: "grow-a-garden-dfdab.firebasestorage.app",
+  messagingSenderId: "868567449799",
+  appId: "1:868567449799:web:b7e28d1e2ff9c7ce53bee2"
+};
